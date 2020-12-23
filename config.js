a4d27a5b0aa9a16fa6ac9703d74574bb92486608
Naif Alanazi <47618283+NaifAlanazi@users.noreply.github.com>
2020-12-24 02:58:06 +0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MONGODB:
-      'mongodb+srv://Naif_Alanazi:Nn055144249188-1@naifapp.xnhnz.mongodb.net/NaifAlanazi?retryWrites=true',
-    SECRET_KEY: 'some very secret key'
-  };
