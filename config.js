cfbb57ff53aa5e93de6e76527f6c0cfaf9d73449
NaifAlanazi <47618283+NaifAlanazi@users.noreply.github.com>
2020-12-23 05:02:34 +0300

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB:
+      'mongodb+srv://Naif_Alanazi:Nn055144249188-1@naifapp.xnhnz.mongodb.net/NaifAlanazi?retryWrites=true',
+    SECRET_KEY: 'some very secret key'
+  };
